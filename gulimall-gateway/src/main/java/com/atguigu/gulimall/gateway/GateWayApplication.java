65ee7268655bb1691a4fb41bb12e6fd1e074a85a
fengjc <1121581401@qq.com>
2020-12-11 15:24:19 +0800

    权限项目加入nacos，配置网关

--- gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GateWayApplication.java
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GateWayApplication.java
@@ -1,5 +1,10 @@
 package com.atguigu.gulimall.gateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * @ClassName GateWayApplication
  * @Description: API网关
@@ -7,8 +12,14 @@ package com.atguigu.gulimall.gateway;
  * @Date 2020/12/9
  * @Version V1.0
  **/
+
+/**
+ * 因为网关服务不涉及到数据库所以不加载Datesour源
+ */
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GateWayApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(GateWayApplication.class);
     }
 }
